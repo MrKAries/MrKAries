a6b36d9b580089d24da98a2d62ee1a93d5868c18
mrkaries <mrkaries@oulook.com>
2022-04-07 10:44:54 +0800

    Initial commit

--- Overall/Layout/src/main.ts
+++ Overall/Layout/src/main.ts
@@ -2,16 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
 
-// qinakun
-import { registerMicroApps } from 'qiankun'
-
-registerMicroApps([
-  {
-    name: 'karies-note',
-    entry: '//localhost:7521',
-    container: '#qiankunContainer',
-    activeRule: '/note',
-  },
-])
-
 createApp(App).use(Router).mount('#karies')
